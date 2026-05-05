20d646e7a2bc94735f4038537c6a31624bd71c48
Newrite <newrite.work@yandex.ru>
2026-05-05 08:58:56 +0700

    temporary remove version from package

--- packages/c/cxxextension/xmake.lua
+++ packages/c/cxxextension/xmake.lua
@@ -1,6 +1,6 @@
 package("cxxextension")
     add_urls("https://github.com/Newrite/CXXExtension.git")
-    add_versions("1.0.0", "v1.0.0")
+    -- add_versions("1.0.0", "v1.0.0")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
