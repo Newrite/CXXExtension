afe57f828e1109dace6b68c4f169d30dbe4b9125
Newrite <newrite.work@yandex.ru>
2026-05-05 09:35:55 +0700

    update xmake.lua for use from source

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,17 @@
 set_xmakever("3.0.8")
 
-set_project("CXXExtension")
-set_version("1.0.0")
-set_defaultmode("releasedbg")
+local is_standalone = os.projectdir() == os.scriptdir()
 
-add_rules("mode.debug", "mode.releasedbg")
-add_rules("plugin.vsxmake.autoupdate")
+if is_standalone then
+    set_project("CXXExtension")
+    set_version("1.0.0")
+    set_defaultmode("releasedbg")
+    add_rules("mode.debug", "mode.releasedbg")
+end
 
 target("CXXExtension")
     set_kind("moduleonly")
+    set_default(is_standalone)
 
     set_languages("c++23")
     set_warnings("allextra")
